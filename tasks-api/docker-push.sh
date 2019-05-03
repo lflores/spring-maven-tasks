1f408d6b6e560b9e9c19ee54fdb8923dd636678e
triad <leonardo.flores@xcaleconsulting.com>
2019-05-03 17:16:03 -0300

    #25 Docker build, fixing region script

--- tasks-api/docker-push.sh
+++ tasks-api/docker-push.sh
@@ -19,7 +19,7 @@ VERSION="${TIMESTAMP}-${TRAVIS_COMMIT}"
 TARGET_IMAGE_VERSIONED="${TARGET_IMAGE}:${VERSION}"
 
 # making sure correct region is set
-aws configure set default.region ${EB_REGION}
+aws configure set default.region ${AWS_REGION}
 
 # Push image to ECR
 ###################
